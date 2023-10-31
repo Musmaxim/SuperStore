68652e3ef8b21fee4b4a90dd28532dafc6ad496f
Mus Maxim <musmaxim.musienko@yandex.ru>
2023-10-31 09:14:12 +0300

    Добавление, реадктирование и удаление стора

--- components/StoreSwitcher.tsx
+++ components/StoreSwitcher.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { Store } from "@prisma/client";
-import { Popover, PopoverTrigger } from "./ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { Store as StoreIcon } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -27,6 +36,10 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
     value: item.id,
   }));
 
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
   const [open, setOpen] = useState(false);
 
   const onStoreSelect = (store: { value: string; label: string }) => {
@@ -45,10 +58,53 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
           aria-label="select a store"
           className={cn("w-[200px] justify-between", className)}
         >
-            Curren Store
           <StoreIcon className="mr-2 h-4 w-4" />
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
+      <PopoverContent>
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="search store..." />
+            <CommandEmpty>No store found</CommandEmpty>
+            <CommandGroup heading="stores">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto w-4 h-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator/>
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+              onSelect={() => {
+                setOpen(false);
+                storeModal.onOpen();
+              }}
+              >
+                <PlusCircle className="mr-5 h-5 w-5"/>
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
     </Popover>
   );
 };
